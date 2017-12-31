756fcacce8024a7b1ffefeb5f5b0566204fa6ad6
zy-mimicry <imcatsee@sina.com>
2017-12-31 11:36:37 +0800

    [git repo] -- init spacemacs configuration

---
+++ custom.el
@@ -0,0 +1,19 @@
+;; Do not write anything past this comment. This is where Emacs will
+;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (prodigy youdao-dictionary names chinese-word-at-point yapfify yaml-mode web-beautify restclient-helm rainbow-mode rainbow-identifiers pyvenv pytest pyenv-mode py-isort pip-requirements ob-restclient ob-http mmm-mode markdown-toc markdown-mode lua-mode livid-mode skewer-mode simple-httpd live-py-mode json-mode json-snatcher json-reformat js2-refactor multiple-cursors js2-mode js-doc ibuffer-projectile hy-mode helm-pydoc deft cython-mode company-tern dash-functional kern company-restclient restclient know-your-http-well company-auctex company-anaconda color-identifiers-mode coffee-mode auctex anaconda-mode pythonic powerline smartparens web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode helm-css-scss haml-mode emmet-mode company-web web-completion-data helm-gtags ggtags flycheck-ycmd company-ycmd ycmd request-deferred deferred xterm-color unfill smeargle shell-pop orgit org-category-capture org-present org-pomodoro alert log4e gntp org-download mwim multi-term magit-gitflow htmlize helm-gitignore gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe git-gutter+ git-gutter flyspell-correct-helm flyspell-correct flycheck-pos-tip flycheck evil-magit magit magit-popup git-commit ghub with-editor eshell-z eshell-prompt-extras esh-help disaster diff-hl company-c-headers cmake-mode clang-format ws-butler winum which-key uuidgen toc-org spaceline restart-emacs request persp-mode org-plus-contrib org-bullets lorem-ipsum link-hint hl-todo hide-comnt help-fns+ helm-flx helm-company fuzzy eyebrowse evil-visual-mark-mode evil-unimpaired evil-mc evil-indent-plus evil-ediff dumb-jump f define-word column-enforce-mode auto-compile packed ace-jump-helm-line helm-c-yasnippet company-statistics company-quickhelp auto-yasnippet ac-ispell auto-complete company yasnippet mwe-log-commands window-numbering smooth-scrolling rfringe page-break-lines leuven-theme ido-vertical-mode hl-anything guide-key-tip gh-md fringe-helper evil-terminal-cursor-changer evil-indent-textobject buffer-move auto-dictionary ace-jump-mode pos-tip guide-key evil-leader volatile-highlights vi-tilde-fringe use-package rainbow-delimiters popwin pcre2el paradox open-junk-file neotree move-text macrostep linum-relative info+ indent-guide hungry-delete highlight-parentheses highlight-numbers highlight-indentation helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-descbinds helm-ag google-translate golden-ratio flx-ido fill-column-indicator fancy-battery expand-region exec-path-from-shell evil-visualstar evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-matchit evil-lisp-state evil-iedit-state evil-exchange evil-escape evil-args evil-anzu eval-sexp-fu elisp-slime-nav clean-aindent-mode auto-highlight-symbol aggressive-indent adaptive-wrap ace-window ace-link)))
+ '(ring-bell-function (quote ignore))
+ '(ycmd-startup-timeout 10))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
