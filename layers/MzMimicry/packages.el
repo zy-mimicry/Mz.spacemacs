004a0fbe7f214c5276c1b2d8e10417ce05a5b4e7
zy-mimicry <imcatsee@sina.com>
2018-01-10 14:34:27 +0800

    [deft] -- notes

--- layers/MzMimicry/packages.el
+++ layers/MzMimicry/packages.el
@@ -63,7 +63,7 @@ Each entry is either:
       - A list beginning with the symbol `recipe' is a melpa
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
 
-(defun MzMimicry/post-init-youdao-dictionary ()
+(defun MzMimicry/init-youdao-dictionary ()
   (use-package youdao-dicrionary
     :defer t
     :init
@@ -97,42 +97,11 @@ Each entry is either:
                   "\\|^#\\+[[:upper:]_]+:.*$" ;; org-mode metadata
                   "\\|^#\\+[[:alnum:]_]+:.*$" ;; org-mode metadata
                   "\\)"))
-    (defun MzMimicry/genfile-timestamp()
-      (concat (format-time-string "%Y_%m%d")
-              (char-to-string (+ 65 (random 26)))
-              (char-to-string (+ 65 (random 26)))
-              "__"))
-
-    ;;advise deft-open-file to replace spaces in file names with _
-    (defun MzMimicry/deft-open-file-advice (orig-fun &rest args)
-      (setq name (pop args))
-      (if (file-exists-p name)
-          (progn
-            (push name args)
-            (apply orig-fun args))
-        (progn
-          (setq title (file-name-sans-extension
-                       (file-name-nondirectory name)))
-          (setq name (concat
-                      (file-name-directory name)
-                      (MzMimicry/genfile-timestamp)
-                      (downcase
-                       (replace-regexp-in-string
-                        " " "_" (file-name-nondirectory name)))
-                      (if (not (file-name-extension name))
-                          ".org")))
-          (push name args)
-          (apply orig-fun args)
-          (insert (concat "#+TITLE: " title "\n\n")))))
-
-    (advice-add 'deft-open-file
-                :around #'MzMimicry/deft-open-file-advice)
 
     (defun MzMimicry/deft-new-file-named-advice (orig-fun &rest args)
       (setq name (pop args))
       (setq title name)
       (setq name (concat
-                  (MzMimicry/genfile-timestamp)
                   (downcase
                    (replace-regexp-in-string
                     " " "_" name))))
@@ -145,63 +114,48 @@ Each entry is either:
 
 (defun MzMimicry/post-init-org ()
 
-  ;; define the refile targets
   (setq org-agenda-file-gtd (expand-file-name "gtd.org" org-agenda-dir))
   (setq org-agenda-file-note (expand-file-name "notes.org" org-agenda-dir))
-  (setq org-agenda-file-journal (expand-file-name "journal.org" org-agenda-dir))
   (setq org-agenda-file-code-snippet (expand-file-name "snippet.org" org-agenda-dir))
   (setq org-agenda-file-daily-english (expand-file-name "english.org" org-agenda-dir))
   (setq org-agenda-files (list org-agenda-dir))
-
   (with-eval-after-load 'org-agenda
     (define-key org-agenda-mode-map (kbd "P") 'org-pomodoro)
     (spacemacs/set-leader-keys-for-major-mode 'org-agenda-mode
       "." 'spacemacs/org-agenda-transient-state/body)
     )
-  ;; the %i would copy the selected text into the template
-  ;;http://www.howardism.org/Technical/Emacs/journaling-org.html
-  ;;add multi-file journal
   (setq org-capture-templates
         '(
-          ;; -----------------------------Just for work
           ("w" "Work.[#A]" entry (file+headline org-agenda-file-gtd "Linux-Android")
            "* TODO [#A] %?\n  %i\n %U"
            :empty-lines 1)
           ("t" "Todo.[#B]" entry (file+headline org-agenda-file-gtd "Workspace-Just-Do")
            "* TODO [#B] %?\n  %i\n"
            :empty-lines 1)
-          ;; -----------------------------Just for life
-          ("b" "Blog Ideas." entry (file+headline org-agenda-file-note "Blog Ideas")
+          ("n" "Note Ideas." entry (file+headline org-agenda-file-note "Note Ideas")
            "* TODO [#B] %?\n  %i\n Date: %U"
            :empty-lines 1)
-          ("l" "Links. " entry (file+headline org-agenda-file-note "Quick notes")
+          ("l" "Links. " entry (file+headline org-agenda-file-note "Quick Links")
            "* TODO [#B] %?\n  %i\n Links: %a \n Date: %U"
-           ;; ----------------------------Just for study
            :empty-lines 1)
-          ("e" "English." entry (file+headline org-agenda-file-daily-english "Quick notes")
+          ("e" "English." entry (file+headline org-agenda-file-daily-english "Quick English")
            "* TODO [C] %?\n \n %U"
            :empty-lines 1)
           ("s" "Code Snippet." entry (file org-agenda-file-code-snippet)
            "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC")
-          ("j" "Journal Entry." entry (file+datetree org-agenda-file-journal)
-           "* %?"
-           :empty-lines 1)))
-
+          )
+        )
   ;;An entry without a cookie is treated just like priority ' B '.
   ;;So when create new task, they are default 重要且紧急
   (setq org-agenda-custom-commands
         '(
-          ("b" "Blog" tags-todo "Mzblog")
-          ("l" "Links" tags-todo "Mzlink")
-          ;; work
           ("w" . "Mz.Org - 工作任务安排")
           ("wa" "重要且紧急的任务" tags-todo "+PRIORITY=\"A\"")
           ("wb" "重要且不紧急的任务" tags-todo "-Weekly-Monthly-Daily+PRIORITY=\"B\"")
           ("wc" "不重要且紧急的任务" tags-todo "+PRIORITY=\"C\"")
-          ;; project for yourself
-          ("p" . "M.Org - 个人项目安排")
-          ("pw" tags-todo "PROJECT+WORK+hCATEGORY=\"linux\"")
-          ("pl" tags-todo "PROJECT+DREAM+CATEGORY=\"project\"")
+          ("p" . "Mz.Org - 个人项目安排")
+          ("pw" "打算学习的标签" tags-todo "PROJECT+WORK+hCATEGORY=\"mzlinux\"")
+          ("pl" "打算开发的项目" tags-todo "PROJECT+DREAM+CATEGORY=\"mzproject\"")
           ("W" "Weekly Review"
            ((stuck "") ;; review stuck projects as designated by org-stuck-projects
             (tags-todo "project") ;; review all projects (assuming you use todo keywords to designate projects)
