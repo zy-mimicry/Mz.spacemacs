ff26f74bc0044ab6b70c7cfd978404d68d5174bf
yang.zheng <yang.zheng@tcl.com>
2018-01-17 09:24:44 +0800

    [keybinding] -- remove

--- layers/MzMimicry/keybindings.el
+++ layers/MzMimicry/keybindings.el
@@ -1,15 +1,6 @@
 ;; Keybindings
 ;; Please use `M-x help' for you!
 
-(defun mimicry/as-vim-control-u ()
-  "Just like vim's C-u in insert mode.
-Note that: Just delete to this line , I think it's useful."
-  (interactive)
-  (progn
-    (kill-region (point) (progn
-                           (beginning-of-line) (point)))))
-(define-key evil-insert-state-map (kbd "C-u") 'mimicry/as-vim-control-u)
-
 (define-key evil-insert-state-map (kbd "C-a") 'move-beginning-of-line)
 (define-key evil-insert-state-map (kbd "C-e") 'move-end-of-line)
 (define-key evil-normal-state-map (kbd "C-a") 'move-beginning-of-line)
