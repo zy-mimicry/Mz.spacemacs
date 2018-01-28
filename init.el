b90a7ea7dacb711f94495e3d3bbd833b08738665
zy-mimicry <imcatsee@sina.com>
2018-01-28 13:32:03 +0800

    [spacemacs] -- update

--- init.el
+++ init.el
@@ -36,7 +36,7 @@ values."
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
-     ;; helm
+     helm
      ivy
      (better-defaults :variables
                       better-defaults-move-to-end-of-code-first nil
